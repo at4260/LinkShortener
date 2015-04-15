ece1ab016221b24339c2db05cbb0f1d51181dcb5
at4260 <at4260@gmail.com>
2015-04-15 12:58:11 -0700

    Added check for generating links with collisions.

--- controller.py
+++ controller.py
@@ -21,12 +21,19 @@ def create_link():
     
     link = m_session.query(model.Link).filter_by(original_link=input_link).first()
 
+    # Checking if link is already in database
     if link is not None:
         shortened_link = link.shortened_link
     else:
         # shortened_link = pbkdf2_sha512.encrypt(
         #     link, salt=b'64', rounds=100000, salt_size=16)
         shortened_link = "http://www.google.com/" + str(randint(0,1000))
+
+        # Checking if shortened link is already in database- possible encryption collisions
+        check_shortened_link = m_session.query(model.Link).filter_by(shortened_link=shortened_link).first()
+        while check_shortened_link is not None:
+            shortened_link = "http://www.google.com/" + str(randint(0,1000))
+            check_shortened_link = m_session.query(model.Link).filter_by(shortened_link=shortened_link).first()
         new_link = model.Link(original_link=input_link, shortened_link=shortened_link)
         m_session.add(new_link)
         m_session.commit()          
